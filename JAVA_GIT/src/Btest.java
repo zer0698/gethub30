9b142bdaa62e2e8e0d762d525a0acd59ab9d9bbb
KO_18 <KO_18@KO_18-PC>
2018-01-02 12:44:07 +0900

    Btest 두번째 수정 A사용자

--- JAVA_GIT/src/Btest.java
+++ JAVA_GIT/src/Btest.java
@@ -4,5 +4,6 @@ public class Btest {
 		System.out.println("Btest 첫번째 커밋!!");
 		System.out.println("Btest 두번째 커밋!! A 사용자 ");
 		System.out.println("Btest 세번째 커밋!! B 사용자 ");
+		System.out.println("Btest 네번째 커밋!! A 사용자 ");
 	}
 }
